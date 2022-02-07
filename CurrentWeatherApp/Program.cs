c5d8a8cfae61acbce21c287c09083e38882b6fd4
jcrowe91 <jcrowe9119@gmail.com>
2022-02-06 21:50:43 -0600

    Finished Exercise

--- CurrentWeatherApp/Program.cs
+++ CurrentWeatherApp/Program.cs
@@ -11,19 +11,16 @@ namespace CurrentWeatherApp
             var openWeather = "http://api.openweathermap.org/data/2.5/weather?lat=33.5207&lon=-86.8025&units=imperial&appid=e75bacb12de59da54c5f71dd08a02b3a";
             var weatherSite = new HttpClient();
             var siteResponse = weatherSite.GetStringAsync(openWeather).Result;
-            var currentWeather = JObject.Parse(siteResponse).GetValue("main");
-
+            JToken currentWeather = JObject.Parse(siteResponse).GetValue("main");
+            
             Console.WriteLine("The current weather stats for Birmingham Alabama are:");
             foreach (var item in currentWeather)
             {
                 Console.WriteLine(item);
                 Console.WriteLine("-------------------------");
             }
-
-            Console.WriteLine();
-
-
-
+            Console.WriteLine("Press Any Key To Exit");
+            Console.ReadLine();
         }
     }
 }
